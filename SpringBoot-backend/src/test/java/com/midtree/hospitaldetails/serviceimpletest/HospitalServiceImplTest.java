c7166bedb1b954ae18fce76d434d1db34964e929
Banid Vinod <bandivinod29@gmail.com>
2022-09-05 11:34:20 +0530

    added service imple test

---
+++ SpringBoot-backend/src/test/java/com/midtree/hospitaldetails/serviceimpletest/HospitalServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.midtree.hospitaldetails.serviceimpletest;
+
+import org.junit.jupiter.api.Test;
+
+public class HospitalServiceImplTest {
+
+	@Test
+	void showDoctorInformationTest()
+	{
+		
+	}
+}
